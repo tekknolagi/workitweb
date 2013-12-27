f188756de9213ce8dccac6013e6662b21fe877b4
Max Bernstein <tekknolagi@gmail.com>
2013-12-26 23:29:55 -1000

    load & compile condensed

--- lib.js
+++ lib.js
@@ -1,35 +1,37 @@
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+}
+
 function idToName(id) {
     return id.split('-').slice(0, -1).join('-');
 }
 
+function compileTemplate(templateName, templateText) {
+    window.templateMap[templateName] = Handlebars.compile(templateText);
+}
+
+function compilePartialTemplate(templateName, templateText) {
+    Handlebars.registerPartial(templateName, templateText);
+}
+
 function loadTemplates() {
     var templateList = $('[type*=x-handlebars]');
+    window.templateMap = {};
     templateList.each(function (ind, val) {
 	var templateName = idToName(val.getAttribute('id'));
 	var templateUrl = 'templates/'+templateName+'.html';
 	console.log('loading template '+templateUrl);
 	$.get(templateUrl, function (data) {
 	    $(val).html(data);
+	    if (templateName.endswith('partial')) {	
+		console.log('compiling partial '+templateName);
+		compilePartialTemplate(templateName, templateText);
+	    }
+	    else {
+		console.log('compiling template '+templateName);
+		compileTemplate(templateName, templateText);
+	    }
 	});
-    });
-}
-
-function compileTemplates() {
-    var templateList = $('[type=text/x-handlebars]');
-    window.templateMap = {};
-    templateList.each(function (ind, val) {
-	var templateText = val.text;
-	var templateName = idToName(val.getAttribute('id'));
-	console.log('compiling template '+templateName);
-	window.templateMap[templateName] = Handlebars.compile(templateText);
-    });
-
-    var partialTemplateList = $('[type=text/x-handlebars-partial]');
-    partialTemplateList.each(function (ind, val) {
-	var templateText = val.text;
-	var templateName = idToName(val.getAttribute('id'));
-	console.log('compiling partial '+templateName);
-	Handlebars.registerPartial(templateName, templateText);
     });
 }
 
