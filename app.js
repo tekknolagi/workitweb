035e26c6f72d1b1c405200071951534dd642cda8
Max Bernstein <tekknolagi@gmail.com>
2013-12-26 18:20:30 -1000

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var templateList = $('.template');
+    var templateMap = {};
+    templateList.each(function (ind, val) {
+	var templateText = val.text;
+	var templateName = val.getAttribute('id').split("-")[0];
+	templateMap[templateName] = Handlebars.compile(templateText);
+    });
+
+    var Template = function(name) {
+	return templateMap[name];
+    }
+
+    var Page = function(page, data) {
+	if (typeof(data) === 'undefined') data = {};
+	return function() { $('#content').html(page(data)) };
+    }
+
+    routie({
+	'register': Page(Template('register'), {name: 'Max'}),
+	'aloha': function() {
+	    $('#greeter').html('Welcome to Hawaii!');
+	},
+	'greet/:name': function(name) {
+	    Page(Template('greeter'), {name: name})();
+	}
+    });
+
+});
