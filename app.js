f172d706e0702c53ae578d4f082345435e02014d
Max Bernstein <tekknolagi@gmail.com>
2013-12-26 22:22:22 -1000

    moved page and template

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
     loadTemplates();
     compileTemplates();
-
-    var Template = function(name) {
-	return window.templateMap[name];
-    }
-
-    var Page = function(page, data) {
-	if (typeof(data) === 'undefined') data = {};
-	return function() { $('#content').html(page(data)) };
-    }
         
     routie({
 	'': Page(Template('home')),
